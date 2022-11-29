0e7986a0c100b272fbca5ec735c69f5aec3def68
Baridian <no@thanks.com>
2022-11-29 17:31:28 -0600

    lexer is working properly

--- lex.rb
+++ lex.rb
@@ -1,19 +1,65 @@
+# frozen_string_literal: true
+
+require './lex_token.rb'
+
 # lexical analyzer.
 # pulls lex rules from lexdefs.txt
 class Lex
-  def parse_lex_rules_file(file)
+  # set up the rules hash. No checking is done to make
+  # sure the file is formatted properly.
+  def initialize(file)
+    @rules_hash = {}
     lines = file.split("\n")
     lines.each do |line|
-      rules_hash[line.match(/\w+(?==)/).to_s] = Regexp.new line.match(/(?<==\/).*(?=\/\w*$)/).to_s
+      pattern = Regexp.new '^' + line.match(%r{(?<==/).*(?=/\w*$)}).to_s
+      @rules_hash[line.match(/\w+(?==)/).to_s] = pattern
     end
   end
 
-  public
-  def initialize(file)
-    @rules_hash = {}
-    @rules = []
+  # run the lexer.
+  # This takes a string containing the contents of the
+  # file to parse and returns an array of LexTokens.
+  #
+  # Implementation: try all rules against the start of the string.
+  # if anything other than exactly one matches, generate and error and abort.
+  # if only one matches, create the token and add to to_return
+  def run(file)
+    file_copy = file.clone
+    to_return = []
+    # reduce down the string until emtpy
+    while file_copy != ''
+      puts file_copy
+      # rules that match the input
+      matches = []
+      # check each rule, add if match
+      @rules_hash.each_key do |key|
+        pattern = @rules_hash[key]
 
-    @rules_hash = parse_lex_rules_file(file)
-    @rules = @rules_hash.keys
+        matches << [file_copy.match(pattern).to_s, key] if file_copy.match? pattern
+      end
+
+      # if invalid match
+      if matches.length != 1
+        if matches.length > 1
+          puts 'ERROR: Ambiguous input. Multiple rules can be applied:'
+          matches.each do |match|
+            puts "#{match[0]} => #{match[1]}"
+          end
+        else
+          puts 'ERROR: No rules for such input:'
+          puts file_copy.match(/.*/).to_s
+        end
+        puts 'please check input and/or lexer rules'
+        to_return = nil
+        file_copy = ''
+      else # valid input
+        match_length = matches[0][0].length
+        file_copy = file_copy[match_length..] # chop the match off the front
+
+        # generate and add the lex token to the stream
+        to_return << LexToken.new(matches[0][1], matches[0][0])
+      end
+    end
+    to_return
   end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
-lex_defs = File.open("lexdefs.txt","r")
+# frozen_string_literal: true
 
+require './lex'
+
+lex_defs = File.read('lexdefs.txt')
 lexer = Lex.new(lex_defs)
+lexemes = lexer.run(File.read('test.txt'))
+puts lexemes
